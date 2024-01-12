2c5b1e0a49c49d1cca651a628c95be7e66f724e3
Sanj0907 <156055597+Sanj0907@users.noreply.github.com>
2024-01-12 14:43:41 +0800

    Update Timer.cs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,1 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
+public class Timer : MonoBehaviour
+{
+    [Serialized] TextMeshProUGUI timerText;
+    [Serialized] float remainingTime;
+
+    void Update()
+    {
+       if(remainingTime > 0 )
+       {
+          remainingTime -= Time.deltaTime;
+       }
+       else if (remainingTime < 0 )
+       {
+           remainingTime = 0;
+           // GameOver();
+           timerText.color = Color.red;
+       }
+       int minutes = Mathf.FloorToInt(elapsedTime/ 60);
+       int seconds = Mathf.FloorToInt(elapsedTime % 60);
+       timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
