084c8cfbde081e43e91d56b35c31ce13c4cafa5c
Sanj0907 <156055597+Sanj0907@users.noreply.github.com>
2024-01-12 15:13:21 +0800

    Update AudioManager.cs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,1 +1,27 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+[Header("---------- Audio Source ----------")]
+[SerializeField] AudioSource musicSource;
+[SerializeField] AudioSource SFXSoure;
+
+[Header("---------- Audio Clip ----------")]
+public AudioClip background;
+public AudioClip death;
+public AudioClip checkpoint;
+public AudioClip wallTouch;
+public AudioClip portalIn;
+public AudioClip portalOut;
+
+private void Start()
+{
+    musicSource.clip = background;
+    musicSource.Play();
+}   
+
+public void PlaySFX(AudioClip clip)
+{
+    SFXSource.PlayOneShot(clip);
+}    
 
