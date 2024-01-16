40beb6cdc520229c4382e038e45d46cd8ec231b9
Sanj0907 <156055597+Sanj0907@users.noreply.github.com>
2024-01-16 16:24:05 +0800

    Update Portal.cs

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,1 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public string destinationSceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LoadScene();
+        }
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(destinationSceneName);
+    }
+}
 
