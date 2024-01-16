e22357489c9a674681f498975fb320260bba1c8a
Sanj0907 <156055597+Sanj0907@users.noreply.github.com>
2024-01-16 17:34:21 +0800

    Update SpawnMan.cs

--- Assets/Scripts/SpawnMan.cs
+++ Assets/Scripts/SpawnMan.cs
@@ -1,1 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class SpawnMan : MonoBehaviour
+{
+  [SerializeField]
+  private GameObject man;
+}
+// Start is called before the first frame update
+void Start()
+{
+    Instantiate(man,transform.position, Quaternion.identity);
+}
