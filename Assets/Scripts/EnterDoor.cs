4ba0dce057f3807498608ed37ff373b36c8fe660
Sanj0907 <156055597+Sanj0907@users.noreply.github.com>
2024-01-16 17:30:01 +0800

    Update EnterDoor.cs

--- Assets/Scripts/EnterDoor.cs
+++ Assets/Scripts/EnterDoor.cs
@@ -1,1 +1,43 @@
+using System.Collections;
+using System.Collection.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterDoor : Monobehaviour
+{
+private bool enterAllowed;
+private string sceneToLoad;
+
+private void OnTriggerEnter2D(Collider2D collision)
+{
+  if (collision.GetComponent<BlueDoor>())
+  {
+    sceneToLoad = "Level2";
+    enterAllowed = true;
+  }
+  else if (collision.GetComponent<BrownDoor>())
+  {
+    sceneToLoad = "Level1";
+    enterAllowed = true;
+  }
+}
+
+private void OnTriggerExit2D(Collider2D collision)
+{
+  if (collision.GetComponent<BlueDoor>() || collision.GetComponent<BrownDoor>())
+  {
+    enterAllowed = false;
+  }
+}
+
+  private void Update()
+  {
+    if (enterAllowed && Input.GetKey(KeyCode.Return))
+    {
+    SceneManager.LoadScene(sceneToLoad);
+    }
+  }
+}
+
+
 
